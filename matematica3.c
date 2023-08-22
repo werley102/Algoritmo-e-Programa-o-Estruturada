ebb5847bcd4d2b9e04c94c983e19d63b11e3017b
werley102 <141765055+werley102@users.noreply.github.com>
2023-08-22 20:11:05 -0300

    Create matematica3.c

---
+++ matematica3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+main() {
+
+float valoreal; 
+float valordolar = 4.94;
+
+printf("\n Informe o Valor em Real a ser convetido em dolar: ");
+scanf("%f",&valoreal); 
+
+valordolar = valoreal * valordolar;
+
+printf("Seu valor convertido é: %.2f", valordolar);
+
+return 0; 
+}
+
+
