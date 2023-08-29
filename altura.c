f0cc1fbf2bba72025715b20cbca96f6818628937
werley102 <141765055+werley102@users.noreply.github.com>
2023-08-29 20:16:32 -0300

    Create altura.c

---
+++ altura.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+float altura;
+printf("Digite sua altura");
+    scanf("%f", &altura);
+  if(altura >= 1.80){
+  printf("Voce tem 1.80 ou mais de altura \n\n");
+    }else{
+    printf("Voce tem menos de 1.80 \n\n");
+    }  
+  return 0;
+}
