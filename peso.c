07639649d0034e35c8571e35f8ab6daaa0700fe5
werley102 <141765055+werley102@users.noreply.github.com>
2023-08-29 20:36:47 -0300

    peso.c

---
+++ peso.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int peso;
+printf("Digite seu peso\n");
+    scanf("%d", &peso);
+  if(peso >= 60){
+  printf("Voce tem 60 kg ou mais",peso);
+    }else{
+    printf("Seu peso e menor que 60 kg",peso);
+    }  
+  return 0;
+}
