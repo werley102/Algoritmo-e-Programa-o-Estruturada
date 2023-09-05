296e3a1dc05a8cab98c89740be70eed5e1cb5d3e
werley102 <141765055+werley102@users.noreply.github.com>
2023-09-05 20:11:12 -0300

    DDD.c

---
+++ DDD.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+ int main() {
+   int dia;
+  printf("Digite o ddd de sua cidade\n");
+   scanf("%d", &dia);
+
+   switch(dia){
+    case 61:
+       printf("\nBrasilia");
+      break;
+    case 71:
+       printf("\nSalvador");
+      break;
+    case 11:
+       printf("\nSao Paulo");
+      break;
+    case 21:
+       printf("\nRio de Janeiro");
+      break;
+    case 32:
+       printf("\nJuiz de fora");
+      break;
+    case 19:
+       printf("\nCampinas");
+      break;
+    case 27:
+       printf("\nVitoria");
+      break;
+    case 31:
+       printf("\nBelo Horizonte");
+      break;
+     
+     default:
+      printf("\numa cidade no Brasil sem identificação");
+   }
+  return 0;
+}
