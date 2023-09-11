bd2fa48e79bd738e52218f79ea813c93b24d272c
werley102 <141765055+werley102@users.noreply.github.com>
2023-09-11 16:32:28 -0300

    Exec4.4.c

---
+++ exec4.4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    double A, B, C;
+
+    printf("Digite três valores A, B e C: ");
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    if (A > B) {
+        double temp = A;
+        A = B;
+        B = temp;
+    }
+
+    if (A > C) {
+        double temp = A;
+        A = C;
+        C = temp;
+    }
+
+    if (B > C) {
+        double temp = B;
+        B = C;
+        C = temp;
+    }
+
+    printf("Valores em ordem decrescente e: %.2lf, %.2lf, %.2lf\n", A, B, C);
+
+    return 0;
+}
